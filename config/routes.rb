652b056978ecaf02c93a6e99955c9fddb5baed1a
satoshishimazaki <sattori06010601@gmail.com>
2014-09-19 00:35:50 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sample4App::Application.routes.draw do
+  root  'static_pages#home'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
